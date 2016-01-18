e49ebd9d4e06dc3d02b8923c3d021c996fcf9175
Nick Gaswirth <nick@roundhouse-designs.com>
2016-01-18 10:22:24 -0500

    Added styles and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rhdwp-related
+
+Don't forget to add a default-thumbnail.jpg in the 'img' directory!
--- rhd-related.php
+++ rhd-related.php
@@ -1,11 +1,49 @@
+<?php
 /**
  * Plugin Name: RHD Related Posts
+ * Description: Simple "related posts" plugin.
  * Author: Roundhouse Designs
  * Author URI: https://roundhouse-designs.com
  * Version: 1.0
  */
 
-function rhd_related_posts() {
+define( 'RHD_REL_DIR', plugin_dir_url(__FILE__) );
+
+
+/**
+ * rhd_related_image_sizes function.
+ *
+ * @access public
+ * @return void
+ */
+function rhd_related_image_sizes()
+{
+	add_image_size( 'square-thumb', 300, 300, true );
+}
+add_action( 'after_setup_theme', 'rhd_related_image_sizes' );
+
+
+/**
+ * rhd_related_enqueue_styles function.
+ *
+ * @access public
+ * @return void
+ */
+function rhd_related_enqueue_styles()
+{
+	wp_enqueue_style( 'rhd-related', RHD_REL_DIR . '/rhd-related.css', null, 1.0, 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
+
+
+/**
+ * rhd_related_posts function.
+ *
+ * @access public
+ * @return void
+ */
+function rhd_related_posts()
+{
 	global $post;
 	$tags = wp_get_post_tags( $post->ID );
 	if ( $tags ) {
@@ -23,22 +61,23 @@ function rhd_related_posts() {
 					. "<h4 class='rhd-related-posts-title'>Related Posts</h4>\n"
 					. "<ul id='rhd-related-posts'>\n";
 
-					foreach ( $related_posts as $post ) : setup_postdata( $GLOBALS['post'] =& $post );
-						$title = get_the_title();
-						$permalink = get_the_permalink();
+			foreach ( $related_posts as $post ) : setup_postdata( $GLOBALS['post'] =& $post );
+				$title = get_the_title();
+				$permalink = get_the_permalink();
 
-						$output .= "<li class='related-post'>\n"
-								. "<a class='related-link' href='$permalink' rel='bookmark'>\n";
+				$output .= "<li class='related-post'>\n"
+						. "<a class='related-link' href='$permalink' rel='bookmark'>\n";
 
-						if ( has_post_thumbnail() ) {
-							$output .= get_the_post_thumbnail( $post->ID, 'square-thumb' );
-						} else {
-							$output .= "<img class='related-thumb-default' src='" . RHD_IMG_DIR . "/default-thumbnail.jpg' alt='$title'>\n";
-						}
+				if ( has_post_thumbnail() ) {
+					$output .= get_the_post_thumbnail( $post->ID, 'square-thumb' );
+				} else {
+					$output .= "<img class='related-thumb-default' src='" . RHD_REL_DIR . "/img/default-thumbnail.jpg' alt='$title'>\n";
+				}
 
-						$output .= "<p class='related-title'>$title</p>\n"
-								. "</a>\n</li>\n";
-					endforeach;
+				$output .= "<p class='related-title'>$title</p>\n"
+						. "</a>\n</li>\n";
+			endforeach;
+
 			$output .= "</ul>\n";
 
 			echo $output;
@@ -46,3 +85,4 @@ function rhd_related_posts() {
 	}
 	wp_reset_postdata();
 }
+?>
