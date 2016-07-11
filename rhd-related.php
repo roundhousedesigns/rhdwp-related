5b1556784609858b9d00f3fc50bcf41d4364f709
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-11 18:46:26 -0400

    styling tweaks and fixed unclosed div

--- rhd-related.php
+++ rhd-related.php
@@ -25,7 +25,7 @@ add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
 
 /**
  * rhd_related_posts function.
- * 
+ *
  * @access public
  * @param string $orderby (default: 'rand')
  * @param mixed $days (default: null) number of days to search back
@@ -35,7 +35,7 @@ function rhd_related_posts( $orderby = 'rand', $days = null )
 {
 	global $post;
 	$tags = wp_get_post_tags( $post->ID );
-	
+
 	if ( $tags ) {
 		foreach( $tags as $tag ) {
 			$tag_arr .= $tag->slug . ',';
@@ -46,16 +46,16 @@ function rhd_related_posts( $orderby = 'rand', $days = null )
 			'post__not_in' => array( $post->ID ),
 			'orderby' => $orderby
 		);
-		
+
 		if ( $days ) {
 			$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
 			$args['date_query'] = array( array( 'after' => $range ) );
 		}
-		
+
 		$related_posts = get_posts( $args );
 		if ( $related_posts ) {
 			$output = "<div id='rhd-related-posts-container'>\n"
-					. "<h4 class='rhd-related-posts-title'>Related Posts</h4>\n"
+					. "<h4 class='rhd-related-posts-title'>You May Also Like...</h4>\n"
 					. "<ul id='rhd-related-posts'>\n";
 
 			foreach ( $related_posts as $post ) : setup_postdata( $GLOBALS['post'] =& $post );
@@ -75,7 +75,7 @@ function rhd_related_posts( $orderby = 'rand', $days = null )
 						. "</a>\n</li>\n";
 			endforeach;
 
-			$output .= "</ul>\n";
+			$output .= "</ul>\n</div>";
 
 			echo $output;
 		}
