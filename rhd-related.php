c9831add7747ba50e42f5326317379222042e6ef
Nick Gaswirth <nick@roundhouse-designs.com>
2018-03-20 11:42:42 -0400

    1.3: -Added transients to save on DB querying; -Killed output buffering and reliance on theme template file; Added no-pin to image class

--- rhd-related.php
+++ rhd-related.php
@@ -4,7 +4,7 @@
  * Description: Simple "related posts" plugin.
  * Author: Roundhouse Designs
  * Author URI: https://roundhouse-designs.com
- * Version: 1.23
+ * Version: 1.3
  */
 
 define( 'RHD_REL_DIR', plugin_dir_url(__FILE__) );
@@ -18,7 +18,7 @@ define( 'RHD_REL_DIR', plugin_dir_url(__FILE__) );
  */
 function rhd_related_enqueue_styles()
 {
-	wp_enqueue_style( 'rhd-related', RHD_REL_DIR . '/rhd-related.css', null, 1.0, 'all' );
+	wp_enqueue_style( 'rhd-related', RHD_REL_DIR . '/rhd-related.css', null, null, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
 
@@ -31,62 +31,66 @@ add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
  * @param mixed $days (default: null)
  * @param int $ppp (default: 4)
  * @param string $text (default: "You May Also Like...")
+ * @param int $expire (default: MONTH_IN_SECONDS) Transient expiration
  * @return void
  */
-function rhd_related_posts( $orderby = 'rand', $days = null, $ppp = 4, $text = "You May Also Like..." )
+function rhd_related_posts( $orderby = 'rand', $days = null, $ppp = 4, $text = "You May Also Like...", $expire = MONTH_IN_SECONDS )
 {
 	global $post;
-	$tags = wp_get_post_tags( $post->ID );
+	
+	// Check to see if a transient has been set for this post, and if not, retrieve the data and set one.
+	if ( false === ( $related_posts = get_transient( 'rhd_related_posts_' . $post->ID ) ) ) {
+		$tags = wp_get_post_tags( $post->ID );
+	
+		$tag_arr = '';
+	
+		if ( $tags ) {
+			foreach( $tags as $tag ) {
+				$tag_arr .= $tag->slug . ',';
+			}
+			$args = array(
+				'tag' => $tag_arr,
+				'posts_per_page' => $ppp,
+				'post__not_in' => array( $post->ID ),
+				'orderby' => $orderby
+			);
+	
+			if ( $days ) {
+				$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
+				$args['date_query'] = array( array( 'after' => $range ) );
+			}
 
-	$tag_arr = '';
-
-	if ( $tags ) {
-		foreach( $tags as $tag ) {
-			$tag_arr .= $tag->slug . ',';
-		}
-		$args = array(
-			'tag' => $tag_arr,
-			'posts_per_page' => $ppp,
-			'post__not_in' => array( $post->ID ),
-			'orderby' => $orderby
-		);
-
-		if ( $days ) {
-			$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
-			$args['date_query'] = array( array( 'after' => $range ) );
-		}
-
-		$related_posts = get_posts( $args );
-		if ( $related_posts ) {
-			$output = "<div id='rhd-related-posts-container'>\n"
-					. "<h4 class='rhd-related-posts-title'>{$text}</h4>\n"
-					. "<ul id='rhd-related-posts'>\n";
-
-			foreach ( $related_posts as $post ) : setup_postdata( $GLOBALS['post'] =& $post );
-				$title = get_the_title();
-				$permalink = get_the_permalink();
-
-				$output .= "<li class='related-post'>\n";
-
-				$output .= "<a href='$permalink' rel='bookmark'>\n";
-
-				if ( has_post_thumbnail() ) {
-					$output .= get_the_post_thumbnail( $post->ID, 'square' );
-				} else {
-					$output .= "<img class='related-thumb-default' src='" . RHD_REL_DIR . "img/default-thumbnail.png' alt='$title'>\n";
-				}
-
-				$output .= "</a>\n";
-
-				$output .= "<p class='related-title'><a class='related-link' href='$permalink' rel='bookmark'>$title</a></p>\n"
-						. "</a>\n</li>\n";
-			endforeach;
-
-			$output .= "</ul>\n</div>";
-
-			echo $output;
+			$related_posts = new WP_Query( $args );
+			set_transient( 'rhd_related_posts_' . $post->ID, $related_posts, $expire );
 		}
 	}
-	wp_reset_postdata();
+	?>	
+	
+	<?php if ( $related_posts && $related_posts->have_posts() ) : ?>
+		<div id="rhd-related-posts-container">
+			<h4 class="rhd-related-posts-title"><?php echo $text; ?></h4>
+			<ul id="rhd-related-posts">
+
+				<?php while( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
+					<li class="related-post">
+						<a href="<?php the_permalink(); ?>" rel="bookmark">
+							<?php
+							if ( has_post_thumbnail() ) {
+								echo get_the_post_thumbnail( $post->ID, 'square' );
+							} else {
+								echo '<img class="related-thumb-default" src="' . RHD_REL_DIR . 'img/default-thumbnail.png" alt="' . get_the_title() . '">';
+							}
+							?>
+						</a>
+		
+						<p class="related-title">
+							<a class="related-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+						</p>
+					</li>
+				<?php endwhile; ?>
+
+			</ul>
+		</div>
+	<?php endif; ?>
+	<?php wp_reset_postdata();
 }
-?>
