2cb42ca79ce12f6d47b70b2e46f9079af711e2d1
Nick Gaswirth <nick@roundhouse-designs.com>
2018-03-20 11:19:32 -0400

    1.3: -Added transients to save on DB querying; -Killed output buffering and reliance on theme template file; Added no-pin to image class

--- rhd-related.php
+++ rhd-related.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: RHD Related Posts
- * Description: Simple "related posts" plugin.
+ * Description: Simple tag-based "related posts" plugin.
  * Author: Roundhouse Designs
  * Author URI: https://roundhouse-designs.com
- * Version: 1.21
+ * Version: 1.3
  */
 
 define( 'RHD_REL_DIR', plugin_dir_url(__FILE__) );
@@ -20,70 +20,83 @@ function rhd_related_enqueue_styles()
 {
 	wp_enqueue_style( 'rhd-related', RHD_REL_DIR . '/rhd-related.css', null, 1.0, 'all' );
 }
-add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
+//add_action( 'wp_enqueue_scripts', 'rhd_related_enqueue_styles' );
 
 
 /**
  * rhd_related_posts function.
  *
  * @access public
- * @param string $orderby (default: 'rand')
+ * @param string $orderby (default: null)
  * @param mixed $days (default: null) number of days to search back
  * @param int $ppp (default: 4) posts per page
+ * @PARAM int $expire (default: MONTH_IN_SECONDS) Transient expiration
  * @return void
  */
-function rhd_related_posts( $orderby = 'rand', $days = null, $ppp = 4 )
+function rhd_related_posts( $orderby = null, $days = null, $ppp = 4, $expire = MONTH_IN_SECONDS )
 {
 	global $post;
-	$tags = wp_get_post_tags( $post->ID );
-
-	if ( $tags ) {
-		foreach( $tags as $tag ) {
-			$tag_arr .= $tag->slug . ',';
-		}
-		$args = array(
-			'tag' => $tag_arr,
-			'posts_per_page' => $ppp,
-			'post__not_in' => array( $post->ID ),
-			'orderby' => $orderby
-		);
-
-		if ( $days ) {
-			$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
-			$args['date_query'] = array( array( 'after' => $range ) );
-		}
-
-		$related_posts = get_posts( $args );
-		if ( $related_posts ) {
-			$output = "<div id='rhd-related-posts-container'>\n"
-					. "<h4 class='rhd-related-posts-title'>You May Also Like...</h4>\n"
-					. "<ul id='rhd-related-posts'>\n";
-
-			foreach ( $related_posts as $post ) : setup_postdata( $GLOBALS['post'] =& $post );
-				$title = get_the_title();
-				$permalink = get_the_permalink();
-
-				$output .= "<li class='related-post'>\n";
-
-				$output .= "<a href='$permalink' rel='bookmark'>\n";
-
-				if ( has_post_thumbnail() ) {
-					$output .= get_the_post_thumbnail( $post->ID, 'square' );
-				} else {
-					$output .= "<img class='related-thumb-default' src='" . RHD_REL_DIR . "img/default-thumbnail.png' alt='$title'>\n";
-				}
-
-				$output .= "</a>\n";
-
-				$output .= "<p class='related-title'><a class='related-link' href='$permalink' rel='bookmark'>$title</a></p>\n"
-						. "</a>\n</li>\n";
-			endforeach;
-
-			$output .= "</ul>\n</div>";
-
-			echo $output;
+	
+	if ( false === ( $related_posts = get_transient( 'rhd_related_post_' . $post->ID ) ) ) {
+		$tags = wp_get_post_tags( $post->ID );
+	
+		$tag_arr = '';
+	
+		if ( $tags ) {
+			foreach( $tags as $tag ) {
+				$tag_arr .= $tag->slug . ',';
+			}
+			$args = array(
+				'tag' => $tag_arr,
+				'posts_per_page' => $ppp,
+				'post__not_in' => array( $post->ID ),
+				'orderby' => $orderby
+			);
+	
+			if ( $days ) {
+				$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
+				$args['date_query'] = array( array( 'after' => $range ) );
+			}
+	
+			$related_posts = new WP_Query( $args );
+			
+			set_transient( 'rhd_related_post_' . $post->ID, $related_posts, $expire );
 		}
 	}
+	?>
+		
+	<?php if ( $related_posts && $related_posts->have_posts() ) : ?>
+		<div id="rhd-related-posts-container">
+			<h4 class='rhd-related-posts-title'>You May Also Like...</h4>
+			<div id='rhd-related-posts' class='post-grid post-container'>
+
+			<?php while ( $related_posts->have_posts() ) : ?>
+				<?php
+				$related_posts->the_post();
+				
+				if ( has_post_thumbnail() ) {
+					$thumb = get_the_post_thumbnail( get_the_ID(), 'square', array( 'alt' => get_the_title() . ' thumbnail', 'class' => 'no-pin' ) );
+				} else {
+					$thumb = wp_get_attachment_image( 26895, 'square', false, array( 'alt' => 'Default thumbnail', 'class' => 'no-pin' ) );
+				}
+				?>
+	
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-grid-item square-grid-item' ); ?>>
+					<div class="entry-thumbnail">
+						<a class="grid-main-link" href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+							<?php echo $thumb; ?>
+						</a>
+				
+						<div class="entry-header">
+							<h2 class="entry-title"><a class="grid-main-link" href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+						</div>
+					</div>
+				</article>
+			<?php endwhile; ?>
+
+			</div>
+		</div>
+		
+	<?php endif;
 	wp_reset_postdata();
 }
-?>
