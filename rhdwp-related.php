58963b55294db7518a76b53747ddd6d13c780370
Nick Gaswirth <nick@roundhouse-designs.com>
2021-04-26 10:39:55 -0400

    template file

--- rhdwp-related.php
+++ rhdwp-related.php
@@ -7,7 +7,8 @@
  * Version: 1.6
  */
 
-define( 'RHDWP_REL_DIR', plugin_dir_url( __FILE__ ) );
+define( 'RHDWP_REL_DIR_URL', plugin_dir_url( __FILE__ ) );
+define( 'RHDWP_REL_DIR', plugin_dir_path( __FILE__ ) );
 
 /**
  * Enqueue styles and scripts
@@ -16,14 +17,13 @@ define( 'RHDWP_REL_DIR', plugin_dir_url( __FILE__ ) );
  * @return void
  */
 function rhdwp_related_enqueue_styles() {
-	wp_enqueue_style( 'rhdwp-related-css', RHDWP_REL_DIR . 'rhdwp-related.css', null, null, 'all' );
+	wp_enqueue_style( 'rhdwp-related-css', RHDWP_REL_DIR_URL . 'rhdwp-related.css', null, null, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'rhdwp_related_enqueue_styles' );
 
-
 /**
  * Main output function
- * 
+ *
  * @access public
  * @param string $orderby (default: 'rand') Ordering
  * @param mixed $days (default: null) Date range
@@ -37,26 +37,26 @@ function rhdwp_related_posts( $orderby = 'rand', $days = null, $ppp = 4, $text =
 
 	// Sanitize $text
 	$text = strip_tags( __( $text, 'rhdwp' ) );
-	
+
 	// Check to see if a transient has been set for this post, and if not, retrieve the data and set one.
 	if ( false === ( $related_posts = get_transient( 'rhdwp_related_posts_' . $post->ID ) ) ) {
 		$tags = wp_get_post_tags( $post->ID );
-	
+
 		$tag_arr = '';
-	
+
 		if ( $tags ) {
-			foreach( $tags as $tag ) {
+			foreach ( $tags as $tag ) {
 				$tag_arr .= $tag->slug . ',';
 			}
 			$args = array(
-				'tag' => $tag_arr,
+				'tag'            => $tag_arr,
 				'posts_per_page' => $ppp,
-				'post__not_in' => array( $post->ID ),
-				'orderby' => $orderby
+				'post__not_in'   => array( $post->ID ),
+				'orderby'        => $orderby,
 			);
-	
+
 			if ( $days ) {
-				$range = date( 'Y-m-d', strtotime( "-{$days} days" ) );
+				$range              = date( 'Y-m-d', strtotime( "-{$days} days" ) );
 				$args['date_query'] = array( array( 'after' => $range ) );
 			}
 
@@ -64,35 +64,24 @@ function rhdwp_related_posts( $orderby = 'rand', $days = null, $ppp = 4, $text =
 			set_transient( 'rhdwp_related_posts_' . $post->ID, $related_posts, $expire );
 		}
 	}
-	?>	
-	
-	<?php if ( $related_posts && $related_posts->have_posts() ) : ?>
-		<div class="rhdwp-related-posts-container">
-			<h4 class="rhdwp-related-posts-title"><?php echo $text; ?></h4>
-			<ul class="rhdwp-related-posts">
 
-				<?php while( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
-					<li class="related-post">
-						<a href="<?php the_permalink(); ?>" rel="bookmark">
-							<?php
-							if ( has_post_thumbnail() ) {
-								echo get_the_post_thumbnail( $post->ID, 'grid', array( 'data-pin-nopin' => 'true' ) );
-							} else {
-								echo '<img class="related-thumb-default" src="' . RHDWP_REL_DIR . 'img/default-thumbnail.png" alt="' . get_the_title() . '" data-pin-nopin="true">';
-							}
-							?>
-						</a>
-		
-						<p class="related-title">
-							<a class="related-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-						</p>
-					</li>
-				<?php endwhile; ?>
+	if ( $related_posts && $related_posts->have_posts() ):
+		printf(
+			'<div class="rhdwp-related-posts-container"><h4 class="rhdwp-related-posts-title">%s</h4><ul class="rhdwp-related-posts">',
+			__( $text, 'rhdwp' )
+		);
 
-			</ul>
-		</div>
-	<?php endif; ?>
-	<?php wp_reset_postdata();
+		while ( $related_posts->have_posts() ): $related_posts->the_post();
+			if ( locate_template( 'rhdwp-related.php' ) ) {
+				get_template_part( 'rhdwp-related' );
+			} else {
+				include RHDWP_REL_DIR . 'template.php';
+			}
+		endwhile;
+
+		printf( '</ul></div>' );
+	endif;
+	wp_reset_postdata();
 }
 
 /**
---
+++ template.php
@@ -0,0 +1,15 @@
+<li class="related-post">
+	<a href="<?php the_permalink(); ?>" rel="bookmark">
+		<?php
+		if ( has_post_thumbnail() ) {
+			echo get_the_post_thumbnail( $post->ID, 'grid', array( 'data-pin-nopin' => 'true' ) );
+		} else {
+			echo '<img class="related-thumb-default" src="' . RHDWP_REL_DIR_URL . 'img/default-thumbnail.png" alt="' . get_the_title() . '" data-pin-nopin="true">';
+		}
+		?>
+	</a>
+
+	<p class="related-title">
+		<a class="related-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+	</p>
+</li>
