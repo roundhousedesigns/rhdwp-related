6c5a7d5cda625f645874c47ee77d10f051085ca6
Nick Gaswirth <admin@roundhouse-designs.com>
2022-03-23 17:36:14 -0400

    Widgets category

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => 'd76510202aa9b49dbae302f9bb20c264');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '2f41f465afede0bb2986cf46b8e8bba3');
