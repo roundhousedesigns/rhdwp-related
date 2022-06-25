dbcaf2914d733d4edfc1f4df6ba3cf2e9cba28d9
Nick Gaswirth <nick@roundhouse-designs.com>
2022-06-25 17:56:18 -0400

    build styles

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => 'd62bdb4d605afc0bf024');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '8b72ddf33242e6edd006');
