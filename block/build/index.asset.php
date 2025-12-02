8c55f1740b8cf794613ebfea550ea7be5696b459
Nick Gaswirth <nick@roundhouse-designs.com>
2025-12-02 14:20:36 -0500

    deps

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '8b72ddf33242e6edd006');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '32bfc8a4b9f25bf46f36');
